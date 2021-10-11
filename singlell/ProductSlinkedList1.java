b5859c4412b4b9e8df60a628c75f4c6625141f17
Kumarsarablaskar <91183837+Kumarsarablaskar@users.noreply.github.com>
2021-10-11 18:42:05 +0530

    Add files via upload

---
+++ singlell/ProductSlinkedList1.java
@@ -0,0 +1,94 @@
+package practiseds.singlell;
+
+public class ProductSlinkedList1 {
+  Product1 head;
+  public static void add(ProductSlinkedList1 stock,Product1 new_product1) {
+	    
+		if(stock.head==null)
+			  stock.head=new_product1;
+		  else {
+			  Product1 trav=stock.head;
+			  while(trav.next!=null)
+				  trav=trav.next;
+			  trav.next=new_product1;
+		  }
+	  }
+	
+  public static void printlist(ProductSlinkedList1 stock) {
+	  Product1 trav;
+	  if(stock.head==null)
+		  System.out.println("the list is empty");
+	  else {
+		  trav=stock.head;
+		  while(trav!=null) {
+			  System.out.print(trav);
+			  trav=trav.next;
+		  }
+	  }
+	  
+  }
+  public static void removeAtIndex(ProductSlinkedList1 stock,int index) {
+	  if(stock.head==null)
+		 System.out.println("The List is Empty..");
+	  else {
+		  int i = 1;
+		  Product1 temp=stock.head;
+		  while(temp.next != null) {
+			  if(i ==index-1) {
+				  temp.next=temp.next.next;
+				  break;
+			  }
+		  temp=temp.next;
+		   i++;
+				 
+			
+		  }
+	  }
+		 
+  }
+  public static void removeLast(ProductSlinkedList1 stock) {
+	  if(stock.head==null)
+			 System.out.println("The List is Empty..");
+	  else {
+		  Product1 temp=stock.head;
+		  while(temp.next==null) {
+			  System.out.print(temp);  
+		  }
+	  }
+  }
+  public static void main(String[] args) {
+	  ProductSlinkedList1 stocklist=new ProductSlinkedList1();
+      stocklist.add(stocklist, new Product1(250, "Maggiee", 12));
+      stocklist.add(stocklist, new Product1(251, "apy",45));
+      stocklist.add(stocklist, new Product1(150, "oreo", 1));
+      printlist(stocklist);
+      System.out.println("\n<---->");
+      stocklist.removeAtIndex(stocklist, 2);
+      printlist(stocklist);
+      System.out.println("\n<---->");
+      stocklist.removeLast(stocklist);
+      printlist(stocklist);
+      
+     
+	
+}
+}
+class Product1{
+	int barcode;
+	String pname;
+	int quantity;
+	Product1 next;
+	public Product1(int barcode, String pname, int quantity) {
+		super();
+		this.barcode = barcode;
+		this.pname = pname;
+		this.quantity = quantity;
+		next=null;
+		
+	}
+	@Override
+	public String toString() {
+		return "Product1 [barcode=" + barcode + ", pname=" + pname + ", quantity=" + quantity + "]\n";
+	}
+	
+}
